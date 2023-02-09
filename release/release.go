985a010f2a8fa5bef34cee1a0008423528aea4be
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-02-09 12:14:23 +0100

    feat: implemented git tag release handling

---
+++ release/release.go
@@ -0,0 +1,137 @@
+package release
+
+import (
+	"fmt"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Masterminds/semver"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+)
+
+type PreReleaseFormat string
+
+const (
+	// PreReleaseFormatSemVer is the default format for prerelease versions.
+	PreReleaseFormatSemVer PreReleaseFormat = "semver"
+	// PreReleaseFormatDate is the format for prerelease versions that use the
+	// current date.
+	PreReleaseFormatDate PreReleaseFormat = "date"
+	// PreReleaseFormatDateTime is the format for prerelease versions that
+	// use the current date and time.
+	PreReleaseFormatDateTime PreReleaseFormat = "datetime"
+)
+
+func (p PreReleaseFormat) String() string {
+	return string(p)
+}
+
+type SemVerBumpType string
+
+const (
+	// SemVerBumpTypeMajor is the bump type for major versions.
+	SemVerBumpTypeMajor SemVerBumpType = "major"
+	// SemVerBumpTypeMinor is the bump type for minor versions.
+	SemVerBumpTypeMinor SemVerBumpType = "minor"
+	// SemVerBumpTypePatch is the bump type for patch versions.
+	SemVerBumpTypePatch SemVerBumpType = "patch"
+)
+
+func (s SemVerBumpType) String() string {
+	return string(s)
+}
+
+// bumpPreRelease bumps the prerelease version of a given semver.Version.
+// The bumping is done according to the given PreReleaseFormat.
+func bumpPreRelease(smvFormat PreReleaseFormat, version semver.Version, preReleasePrefix string) string {
+	tagPrefix := ""
+	if strings.HasPrefix(version.Original(), "v") {
+		tagPrefix = "v"
+	}
+	tagPrefix += fmt.Sprintf("%d.%d.%d", version.Major(), version.Minor(), version.Patch())
+
+	switch smvFormat {
+	case PreReleaseFormatSemVer:
+		if version.Prerelease() == "" {
+			return fmt.Sprintf("%s-%s.%s", tagPrefix, preReleasePrefix, "1")
+		}
+		intVers, err := strconv.Atoi(strings.TrimPrefix(version.Prerelease(), preReleasePrefix+"."))
+		if err != nil {
+			return fmt.Sprintf("%s.%d", version.String(), 1)
+		}
+		return fmt.Sprintf("%s-%s.%d", tagPrefix, preReleasePrefix, intVers+1)
+	case PreReleaseFormatDate:
+		return fmt.Sprintf("%s-%s.%s", tagPrefix, preReleasePrefix, time.Now().Format("20060102"))
+	case PreReleaseFormatDateTime:
+		return fmt.Sprintf("%s-%s.%s", tagPrefix, preReleasePrefix, time.Now().Format("200601021504"))
+	}
+	return fmt.Sprintf("%s-%s.%d", tagPrefix, preReleasePrefix, 1)
+}
+
+func GetLatestSemVerTagFromRepoPath(repo *git.Repository, semverBumpType SemVerBumpType) (*semver.Version, error) {
+	// get tags from repository
+	tags, err := repo.Tags()
+	if err != nil {
+		return nil, err
+	}
+	// get tags from repository
+	vs := []*semver.Version{}
+	tags.ForEach(func(t *plumbing.Reference) error {
+		gitTag := strings.TrimPrefix(t.Name().String(), "refs/tags/")
+		// check if tag matches semver format
+		isSemver := regexp.MustCompile("^[v]{0,1}[0-9]{1,}.[0-9]{1,}.[0-9]{1,}(-[a-zA-Z0-9.-]+){0,1}$").MatchString(gitTag)
+		if !isSemver {
+			return nil
+		}
+		//
+		smv, err := semver.NewVersion(gitTag)
+		if err != nil {
+			return err
+		}
+		vs = append(vs, smv)
+		return nil
+	})
+	// sort tags
+	sort.Sort(semver.Collection(vs))
+
+	// get latest version
+	var latest *semver.Version
+	if len(vs) > 0 {
+		if vs[len(vs)-1].Prerelease() != "" {
+			latest = vs[len(vs)-1]
+		} else {
+			for i := len(vs) - 1; i >= 0; i-- {
+				if vs[i].Prerelease() == "" {
+					latest = vs[i]
+					break
+				}
+			}
+		}
+	}
+
+	return latest, nil
+}
+
+// BumpVersion takes a semver.Version and a semVerBumpType and returns the
+// bumped version as a string.
+func BumpVersion(latest *semver.Version, semVerType SemVerBumpType, preReleaseFormat PreReleaseFormat, preReleasePrefix string, isPreRelease bool) string {
+	newTag := semver.Version{}
+	switch semVerType {
+	case SemVerBumpTypeMajor:
+		newTag = latest.IncMajor()
+	case SemVerBumpTypeMinor:
+		newTag = latest.IncMinor()
+	case SemVerBumpTypePatch:
+		newTag = latest.IncPatch()
+	}
+	// if pre-release is enabled, bump the pre-release version
+	if isPreRelease {
+		vrs := bumpPreRelease(preReleaseFormat, newTag, preReleasePrefix)
+		newTag = *semver.MustParse(vrs)
+	}
+	return newTag.String()
+}
---
+++ release/release_test.go
@@ -0,0 +1,319 @@
+package release
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/Masterminds/semver"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/storage/memory"
+)
+
+func TestPreReleaseFormat_String(t *testing.T) {
+	tests := []struct {
+		name string
+		p    PreReleaseFormat
+		want string
+	}{
+		{
+			name: "PreReleaseFormatSemVer",
+			p:    PreReleaseFormatSemVer,
+			want: "semver",
+		},
+		{
+			name: "PreReleaseFormatDate",
+			p:    PreReleaseFormatDate,
+			want: "date",
+		},
+		{
+			name: "PreReleaseFormatDateAndTime",
+			p:    PreReleaseFormatDateTime,
+			want: "datetime",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.p.String(); got != tt.want {
+				t.Errorf("PreReleaseFormat.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_semVerBumpType_String(t *testing.T) {
+	tests := []struct {
+		name string
+		s    SemVerBumpType
+		want string
+	}{
+		{
+			name: "SemVerBumpTypeMajor",
+			s:    SemVerBumpTypeMajor,
+			want: "major",
+		},
+		{
+			name: "SemVerBumpTypeMinor",
+			s:    SemVerBumpTypeMinor,
+			want: "minor",
+		},
+		{
+			name: "SemVerBumpTypePatch",
+			s:    SemVerBumpTypePatch,
+			want: "patch",
+		},
+		{
+			name: "SemVerBumpTypePatch",
+			s:    "",
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.String(); got != tt.want {
+				t.Errorf("semVerBumpType.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_bumpPreRelease(t *testing.T) {
+	type args struct {
+		smvFormat        PreReleaseFormat
+		version          semver.Version
+		preReleasePrefix string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "PreReleaseFormatSemVer with alpha prerelease",
+			args: args{
+				smvFormat:        PreReleaseFormatSemVer,
+				version:          *semver.MustParse("1.0.0-alpha.1"),
+				preReleasePrefix: "alpha",
+			},
+			want: "1.0.0-alpha.2",
+		},
+		{
+			name: "PreReleaseFormatSemVer",
+			args: args{
+				smvFormat:        PreReleaseFormatSemVer,
+				version:          *semver.MustParse("1.0.0"),
+				preReleasePrefix: "alpha",
+			},
+			want: "1.0.0-alpha.1",
+		},
+		{
+			name: "PreReleaseFormatDate",
+			args: args{
+				smvFormat:        PreReleaseFormatDate,
+				version:          *semver.MustParse("1.0.0"),
+				preReleasePrefix: "alpha",
+			},
+			want: fmt.Sprintf("1.0.0-alpha.%s", time.Now().Format("20060102")),
+		},
+		{
+			name: "PreReleaseFormatDateAndTime",
+			args: args{
+				smvFormat:        PreReleaseFormatDateTime,
+				version:          *semver.MustParse("1.0.0"),
+				preReleasePrefix: "alpha",
+			},
+			want: fmt.Sprintf("1.0.0-alpha.%s", time.Now().Format("200601021504")),
+		},
+		//
+		//
+		//
+		{
+			name: "PreReleaseFormatSemVer with v prefix",
+			args: args{
+				smvFormat:        PreReleaseFormatSemVer,
+				version:          *semver.MustParse("v1.0.0"),
+				preReleasePrefix: "alpha",
+			},
+			want: "v1.0.0-alpha.1",
+		},
+		{
+			name: "PreReleaseFormatDate with v prefix",
+			args: args{
+				smvFormat:        PreReleaseFormatDate,
+				version:          *semver.MustParse("v1.0.0"),
+				preReleasePrefix: "alpha",
+			},
+			want: fmt.Sprintf("v1.0.0-alpha.%s", time.Now().Format("20060102")),
+		},
+		{
+			name: "PreReleaseFormatDateAndTime with v prefix",
+			args: args{
+				smvFormat:        PreReleaseFormatDateTime,
+				version:          *semver.MustParse("v1.0.0"),
+				preReleasePrefix: "alpha",
+			},
+			want: fmt.Sprintf("v1.0.0-alpha.%s", time.Now().Format("200601021504")),
+		},
+
+		{
+			name: "empty",
+			args: args{
+				smvFormat:        "",
+				version:          *semver.MustParse("v1.0.0"),
+				preReleasePrefix: "alpha",
+			},
+			want: "v1.0.0-alpha.1",
+		},
+
+		{
+			name: "PreReleaseFormatSemVer with alpha prerelease and not a number",
+			args: args{
+				smvFormat:        PreReleaseFormatSemVer,
+				version:          *semver.MustParse("1.0.0-alpha.abc"),
+				preReleasePrefix: "alpha",
+			},
+			want: "1.0.0-alpha.abc.1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := bumpPreRelease(tt.args.smvFormat, tt.args.version, tt.args.preReleasePrefix); got != tt.want {
+				t.Errorf("bumpPreRelease() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getLatestSemVerTagFromRepoPath(t *testing.T) {
+	type args struct {
+		repo           *git.Repository
+		semverBumpType SemVerBumpType
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *semver.Version
+		wantErr bool
+	}{
+		{
+			name: "no tags",
+			args: args{
+				repo: func() *git.Repository {
+					repo, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+						URL:  "https://github.com/leonsteinhaeuser/observer",
+						Tags: git.AllTags,
+					})
+					if err != nil {
+						t.Fatal(err)
+					}
+					return repo
+				}(),
+				semverBumpType: SemVerBumpTypeMajor,
+			},
+			want:    semver.MustParse("2.0.1"),
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetLatestSemVerTagFromRepoPath(tt.args.repo, tt.args.semverBumpType)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getLatestSemVerTagFromRepoPath() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if got.String() != tt.want.String() {
+				t.Errorf("getLatestSemVerTagFromRepoPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_bumpVersion(t *testing.T) {
+	type args struct {
+		latest           *semver.Version
+		semVerType       SemVerBumpType
+		preReleaseFormat PreReleaseFormat
+		preReleasePrefix string
+		isPreRelease     bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "patch",
+			args: args{
+				latest:           semver.MustParse("1.0.0"),
+				semVerType:       SemVerBumpTypePatch,
+				preReleaseFormat: PreReleaseFormatSemVer,
+				preReleasePrefix: "alpha",
+				isPreRelease:     false,
+			},
+			want: "1.0.1",
+		},
+		{
+			name: "patch pre-release",
+			args: args{
+				latest:           semver.MustParse("1.0.0"),
+				semVerType:       SemVerBumpTypePatch,
+				preReleaseFormat: PreReleaseFormatSemVer,
+				preReleasePrefix: "alpha",
+				isPreRelease:     true,
+			},
+			want: "1.0.1-alpha.1",
+		},
+		{
+			name: "minor",
+			args: args{
+				latest:           semver.MustParse("1.0.0"),
+				semVerType:       SemVerBumpTypeMinor,
+				preReleaseFormat: PreReleaseFormatSemVer,
+				preReleasePrefix: "alpha",
+				isPreRelease:     false,
+			},
+			want: "1.1.0",
+		},
+		{
+			name: "minor pre-release",
+			args: args{
+				latest:           semver.MustParse("1.0.0"),
+				semVerType:       SemVerBumpTypeMinor,
+				preReleaseFormat: PreReleaseFormatSemVer,
+				preReleasePrefix: "alpha",
+				isPreRelease:     true,
+			},
+			want: "1.1.0-alpha.1",
+		},
+		{
+			name: "major",
+			args: args{
+				latest:           semver.MustParse("1.0.0"),
+				semVerType:       SemVerBumpTypeMajor,
+				preReleaseFormat: PreReleaseFormatSemVer,
+				preReleasePrefix: "alpha",
+				isPreRelease:     false,
+			},
+			want: "2.0.0",
+		},
+		{
+			name: "major",
+			args: args{
+				latest:           semver.MustParse("1.0.0"),
+				semVerType:       SemVerBumpTypeMajor,
+				preReleaseFormat: PreReleaseFormatSemVer,
+				preReleasePrefix: "alpha",
+				isPreRelease:     true,
+			},
+			want: "2.0.0-alpha.1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := BumpVersion(tt.args.latest, tt.args.semVerType, tt.args.preReleaseFormat, tt.args.preReleasePrefix, tt.args.isPreRelease); got != tt.want {
+				t.Errorf("bumpVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
