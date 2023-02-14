617d70ef58fe95009a94910ce913003baf67cce0
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-02-14 23:07:05 +0100

    feat: added config path and auto bump cmd flag

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	_ "embed"
 	"flag"
 	"fmt"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/leonsteinhaeuser/git-tag-identifier/branch"
 	"github.com/leonsteinhaeuser/git-tag-identifier/release"
+	"gopkg.in/yaml.v3"
 )
 
 var (
@@ -14,10 +17,30 @@ var (
 	bumpType         = flag.String("bump", release.SemVerBumpTypePatch.String(), "Bump type (major, minor, patch")
 	isPreRelease     = flag.Bool("pre-release", false, "Whether to create a pre-release")
 	repoTarget       = flag.String("repo-path", ".", "Path to the repository")
+	configPath       = flag.String("config", "", "Path to the config file")
+	autoBump         = flag.Bool("auto-bump", false, "Whether to automatically bump the version based on the rules in the config file")
+
+	//go:embed config.yaml
+	configBts []byte
+	// embed default config during build
+	config *branch.Config
 )
 
 func init() {
 	flag.Parse()
+	// if config flag is set, read config from file
+	if *configPath != "" {
+		cfg, err := branch.ReadConfig(*configPath)
+		if err != nil {
+			panic(err)
+		}
+		config = cfg
+	} else {
+		err := yaml.Unmarshal(configBts, &config)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func main() {
@@ -31,9 +54,18 @@ func main() {
 		panic(err)
 	}
 
+	bt := release.SemVerBumpType(*bumpType)
+	if *autoBump {
+		identifier, err := branch.Identify(config, repo)
+		if err != nil {
+			panic(err)
+		}
+		bt = identifier
+	}
+
 	newTag := release.BumpVersion(
 		latest,
-		release.SemVerBumpType(*bumpType),
+		bt,
 		release.PreReleaseFormat(*preReleaseFormat),
 		*preReleasePrefix,
 		*isPreRelease,
