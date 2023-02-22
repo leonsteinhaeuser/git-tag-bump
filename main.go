2dad6ffb258d5af6dffed4f0f699ee3db5eb4d7f
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-02-22 21:38:49 +0100

    enhancement: aligned pre release flags to --pre-release-xxx

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 )
 
 var (
-	preReleaseFormat = flag.String("prerelease-format", release.PreReleaseFormatSemVer.String(), "Prerelease format. Can be 'semver', 'date' or 'datetime'")
-	preReleasePrefix = flag.String("prerelease-prefix", "rc", "Prerelease prefix")
+	preReleaseFormat = flag.String("pre-release-format", release.PreReleaseFormatSemVer.String(), "Prerelease format. Can be 'semver', 'date' or 'datetime'")
+	preReleasePrefix = flag.String("pre-release-prefix", "rc", "Prerelease prefix")
 	bumpType         = flag.String("bump", release.SemVerBumpTypePatch.String(), "Bump type (major, minor, patch")
 	isPreRelease     = flag.Bool("pre-release", false, "Whether to create a pre-release")
 	repoTarget       = flag.String("repo-path", ".", "Path to the repository")
