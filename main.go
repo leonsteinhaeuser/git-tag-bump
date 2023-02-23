029bd86343dbde8753283272fb93a401cb41c4c1
Leon Steinhäuser <42437185+leonsteinhaeuser@users.noreply.github.com>
2023-02-23 01:08:32 +0100

    feat: pass branch name as string (#5)
    * refactor: made IdentifyBranch a public function
    
    * feat: added branch name flag and version auto detect

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ var (
 	repoTarget       = flag.String("repo-path", ".", "Path to the repository")
 	configPath       = flag.String("config", "", "Path to the config file")
 	autoBump         = flag.Bool("auto-bump", false, "Whether to automatically bump the version based on the rules in the config file")
+	branchName       = flag.String("branch-name", "", "Name of the branch to check")
 	vPrefix          = flag.Bool("v-prefix", true, "Whether to prefix the tag with a 'v'. E.g. v1.0.0 instead of 1.0.0")
 
 	//go:embed config.yaml
@@ -56,12 +57,19 @@ func main() {
 	}
 
 	bt := release.SemVerBumpType(*bumpType)
-	if *autoBump {
+	if *autoBump && *branchName == "" {
 		identifier, err := branch.Identify(config, repo)
 		if err != nil {
 			panic(err)
 		}
 		bt = identifier
+	}
+	if *branchName != "" {
+		smvTag, err := branch.IdentifyBranch(config, *branchName)
+		if err != nil {
+			panic(err)
+		}
+		bt = smvTag
 	}
 
 	newTag := release.BumpTag(
