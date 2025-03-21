93d7625121cb4aae162faea0c803cb9f69ffcc6d
Stuart Auld <sja@marsupialmusic.net>
2025-03-22 04:09:31 +1100

    feat: add support to create lightweight tags (#21)
    add support to create lightweight tags

--- main.go
+++ main.go
@@ -18,17 +18,18 @@ import (
 )
 
 var (
-	preReleaseFormat   = flag.String("pre-release-format", release.PreReleaseFormatSemVer.String(), "Prerelease format. Can be 'semver', 'date' or 'datetime'")
-	preReleasePrefix   = flag.String("pre-release-prefix", "rc", "Prerelease prefix")
-	bumpType           = flag.String("bump", release.SemVerBumpTypePatch.String(), "Bump type (major, minor, patch, none)")
-	isPreRelease       = flag.Bool("pre-release", false, "Whether to create a pre-release")
-	repoTarget         = flag.String("repo-path", ".", "Path to the repository")
-	configPath         = flag.String("config", "", "Path to the config file")
-	autoBump           = flag.Bool("auto-bump", false, "Whether to automatically bump the version based on the rules in the config file")
-	createTag          = flag.Bool("create", false, "Whether to create a tag in the repository and push it to the remote")
-	branchName         = flag.String("branch-name", "", "Name of the branch to check")
-	vPrefix            = flag.Bool("v-prefix", true, "Whether to prefix the tag with a 'v'. E.g. v1.0.0 instead of 1.0.0")
-	gitBaseTagOverride = flag.String("git-base-tag", "", "Override the base tag to use for the bump. If not set, the latest tag will be used.")
+	preReleaseFormat     = flag.String("pre-release-format", release.PreReleaseFormatSemVer.String(), "Prerelease format. Can be 'semver', 'date' or 'datetime'")
+	preReleasePrefix     = flag.String("pre-release-prefix", "rc", "Prerelease prefix")
+	bumpType             = flag.String("bump", release.SemVerBumpTypePatch.String(), "Bump type (major, minor, patch, none)")
+	isPreRelease         = flag.Bool("pre-release", false, "Whether to create a pre-release")
+	repoTarget           = flag.String("repo-path", ".", "Path to the repository")
+	configPath           = flag.String("config", "", "Path to the config file")
+	autoBump             = flag.Bool("auto-bump", false, "Whether to automatically bump the version based on the rules in the config file")
+	createTag            = flag.Bool("create", false, "Whether to create a tag in the repository and push it to the remote")
+	createTagLightweight = flag.Bool("lightweight", false, "Whether any tag created should be a lightweight tag")
+	branchName           = flag.String("branch-name", "", "Name of the branch to check")
+	vPrefix              = flag.Bool("v-prefix", true, "Whether to prefix the tag with a 'v'. E.g. v1.0.0 instead of 1.0.0")
+	gitBaseTagOverride   = flag.String("git-base-tag", "", "Override the base tag to use for the bump. If not set, the latest tag will be used.")
 
 	actorName = flag.String("actor-name", "", "The name of the actor used to create the tag. Only used if --create is set.")
 	actorMail = flag.String("actor-mail", "", "The mail of the actor used to create the tag. Only used if --create is set.")
@@ -59,8 +60,8 @@ func init() {
 		}
 	}
 
-	if *createTag && (*actorName == "" || *actorMail == "" || githubToken == "") {
-		panic("Both --actor-name and --actor-mail must be set when --create is set")
+	if *createTag && !*createTagLightweight && (*actorName == "" || *actorMail == "" || githubToken == "") {
+		panic("Either --lightweight, or both --actor-name and --actor-mail must be set when --create is set")
 	}
 
 	if *createTag && *actorName != "" && *actorMail != "" {
@@ -124,17 +125,23 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+
+		var options *git.CreateTagOptions
+		if !*createTagLightweight {
+			options = &git.CreateTagOptions{
+				//Tagger:  nil,
+				Message: newTag,
+				Tagger: func() *object.Signature {
+					if actor.Name == "" || actor.Email == "" {
+						return nil
+					}
+					return actor
+				}(),
+			}
+		}
+
 		// create the tag in the repository for the current commit hash
-		pmbrfc, err := repo.CreateTag(newTag, rfc.Hash(), &git.CreateTagOptions{
-			//Tagger:  nil,
-			Message: newTag,
-			Tagger: func() *object.Signature {
-				if actor.Name == "" || actor.Email == "" {
-					return nil
-				}
-				return actor
-			}(),
-		})
+		pmbrfc, err := repo.CreateTag(newTag, rfc.Hash(), options)
 		if err != nil {
 			panic(fmt.Sprintf("Could not create tag: %q, exited with error: %s", newTag, err))
 		}
